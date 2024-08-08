5a32c5ed80663ead173214829d86fb1060f22159
samfdhanani <165212066+samfdhanani@users.noreply.github.com>
2024-08-08 12:05:16 -0400

    Update Raster_All_GTandSex.py

--- Raster_All_GTandSex.py
+++ Raster_All_GTandSex.py
@@ -5,26 +5,24 @@ import math
 from matplotlib.backends.backend_pdf import PdfPages
 import os
 
-# Read data from CSV file
 csv_file = "/Users/samdhanani/Desktop/MuhleLab/Operant_Data_Folders/CohF_social_trainingburst2-3s_030724.csv"
-target_date = "06-05-24"  # Specify the target date you want to plot
-max_x_value = 2500  # Maximum value for the x-axis
+date = "06-05-24"  # Specify the date you want to plot
+max_x_value = 2500 
 
 pdf_file_path = "/Users/samdhanani/Desktop/MuhleLab/CohF_RasterPlot/06-05-24.pdf"
 pdf_pages = PdfPages(pdf_file_path)
 
 with open(csv_file, 'r') as file:
     reader = csv.DictReader(file)
-    genotype_sex_dict = {}  # To store subjects grouped by genotype and sex
+    genotype_sex_dict = {}  # 
 
     for row in reader:
-        if row['Date'] == target_date:
+        if row['Date'] == date:
             genotype_sex = (row['Genotype'], row['Sex'])
             if genotype_sex not in genotype_sex_dict:
                 genotype_sex_dict[genotype_sex] = []
             genotype_sex_dict[genotype_sex].append(row)
 
-    # Open a PDF file
     with PdfPages(pdf_file_path) as pdf_pages:
         # Generate plots for each group of genotype and sex
         for genotype_sex, subjects in genotype_sex_dict.items():
@@ -32,7 +30,7 @@ with open(csv_file, 'r') as file:
             num_cols = min(len(subjects), 2)
             fig, axs = plt.subplots(num_rows, num_cols, figsize=(20, 10 * num_rows))
             genotype, sex = genotype_sex
-            fig.suptitle(f'{genotype} - {sex} - {target_date}', fontsize=14)
+            fig.suptitle(f'{genotype} - {sex} - {date}', fontsize=14)
 
             for i, subject in enumerate(subjects):
                 event_occurrences = ast.literal_eval(subject['plot'])
@@ -44,7 +42,7 @@ with open(csv_file, 'r') as file:
                 axs[row_idx, col_idx].set_title(subject['Subject'])
                 axs[row_idx, col_idx].set_yticks([])
                 axs[row_idx, col_idx].grid(True)
-                axs[row_idx, col_idx].set_xlim(0, max_x_value)  # Set maximum x-axis value
+                axs[row_idx, col_idx].set_xlim(0, max_x_value)
 
             # Adjust subplot spacing
             fig.subplots_adjust(hspace=0.7, wspace=0.08, left=0.015, right=0.987)
